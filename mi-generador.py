31ddf98e7ad22258c6fd231daa2e1300985c1e8f
Santi-Ronchi <sronchi@fi.uba.ar>
2025-08-31 12:54:03 -0300

    adding AGENCIA=1 to client environment

--- mi-generador.py
+++ mi-generador.py
@@ -36,6 +36,7 @@ with open(archivo, "w") as f:
         f.write("    entrypoint: /client\n")
         f.write("    environment:\n")
         f.write(f"      - CLI_ID={i}\n")
+        f.write(f"      - AGENCIA=1\n")
         f.write("    networks:\n")
         f.write("      - testing_net\n")
         f.write("    depends_on:\n")
